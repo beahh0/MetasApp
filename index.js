0dd37069fc66a1dcedecef7df2a8730abaac3f0c
Beatriz Gibaut <beahhtriz01@gmail.com>
2024-09-10 18:19:54 -0300

    fundamento 01

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let meta = {
+    value: 'ler um livro',
+    checked: false
+}
+console.log(meta.value)
+
+let metas = [
+    meta,
+    {
+        value: 'ir para a academia',
+        checked: true
+    }
+]
+
+console.log(metas[1].value)
